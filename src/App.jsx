7c43847f5ae74f2a6da9c238c762ba341036e129
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-11-08 00:13:06 -0300

    created logotipo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './styles.css'
+import './global.css'
 import { Header } from "./components/Header"
 
 export function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css'
 
+import IgniteLogo from '../assets/ignite-logo.svg'
+
 export function Header(){
     return (
         <header className={styles.header}>
-            <strong>Ignite Feed</strong>
+            <img src={IgniteLogo} alt="Ignite logo" />
         </header>
     )
 }
