393a63a7565e7aa87b63be0fd3d470fec7d33bc0
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-11-12 23:40:08 -0300

    created posts dinamic

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,42 @@ import { Post } from './components/Post'
 
 import styles from './App.module.css'
 
+
+const posts  = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/rdg-404.png',
+      name: 'Rodrigo Paiva',
+      role: 'Rocketseat Studant'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-11-09 23:50:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-11-12 23:50:00'),
+  },
+  
+]
+
+
 export function App() {
-
-
 
   return (
     <div>
@@ -15,10 +48,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <div>
-            <Post />
-            <Post />
-          </div> 
+          {/* percorre o array e return cada post */}
+          {posts.map(post => {
+            return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+            )
+          })}
         </main>
       </div>
     </div>
