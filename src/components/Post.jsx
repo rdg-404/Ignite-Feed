189c727403dce3236c6a72348b76bbbef529971e
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-11-14 22:17:33 -0300

    formatted date and content from post, dinamically with const

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,49 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post(props){
+export function Post({author, publishedAt, content}){
 
-    console.log(props)
+    //formatado que será mostrado a data
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    });
+
+    // calcula e mostra o tempo relativo ao que foi postado
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR, 
+        addSuffix: true, //add às antes da data
+    });
+
+
 
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/rdg-404.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Rodrigo Paiva</strong>
-                        <span>FrontEnd Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="09 de Novembro às 23:50:00" dateTime='2022-11-09 23:50:12'>Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p><a href="">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto </a> {' '}
-                    <a href="">#nlw </a>{' '}
-                    <a href="">#rocketseat </a>
-                </p>
+                {content.map(item => {
+                    if(item.type === 'paragraph'){
+                        return <p>{item.content}</p>;
+                    }
+                    else if (item.type === 'link'){
+                        return <p><a href="#">{item.content}</a></p>
+                    }   
+                })}
             </div>
 
 
